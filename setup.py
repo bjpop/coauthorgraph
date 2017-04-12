e56972f32b0e36b64ce91ed15f415e44d34d3919
Bernie Pope <bjpope@unimelb.edu.au>
2017-04-12 18:02:35 +1000

    First version of program

--- setup.py
+++ setup.py
@@ -3,27 +3,22 @@
 from distutils.core import setup
 
 LONG_DESCRIPTION = \
-'''The program reads one or more input FASTA files. 
-For each file it computes a variety of statistics, and then
-prints a summary of the statistics as output.
-        
-The goal is to provide a solid foundation for new bioinformatics command line tools,
-and is an ideal starting place for new projects.'''
+'''Read a bibtex file and generate a graph of co-authorship'''
 
 
 setup(
-    name='coauthgraph-py',
+    name='coauthgraph',
     version='0.1.0.0',
     author='Bernie Pope',
     author_email='bjpope@unimelb.edu.au',
     packages=['coauthgraph'],
     package_dir={'coauthgraph': 'coauthgraph'},
     entry_points={
-        'console_scripts': ['coauthgraph-py = coauthgraph.coauthgraph:main']
+        'console_scripts': ['coauthgraph = coauthgraph.coauthgraph:main']
     },
     url='https://github.com/bjpop/coauthgraph',
     license='LICENSE',
-    description=('A prototypical bioinformatics command line tool'),
+    description=('Read a bibtex file and generate a graph of co-authorship'),
     long_description=(LONG_DESCRIPTION),
-    install_requires=["biopython==1.66"],
+    install_requires=["bibtexparser"],
 )
